17664e4058cddaf80ea283f92f5671fb98394ebb
Hallison Melo <hallison.pm@gmail.com>
2022-08-10 14:13:06 -0300

    start project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class HomeView extends Vue {}
+</script>
